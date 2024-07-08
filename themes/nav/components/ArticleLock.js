2309abbe1b467bb0e64491795abcbaca67e27bbd
Waddle <141616559+W4ddl3@users.noreply.github.com>
2024-07-08 12:17:58 +0800

    Update ArticleLock.js

--- themes/nav/components/ArticleLock.js
+++ themes/nav/components/ArticleLock.js
@@ -31,7 +31,7 @@ export const ArticleLock = props => {
 
   return <div id='container' className='w-full flex justify-center items-center h-96 '>
     <div className='text-center space-y-3'>
-      <div className='font-bold'>关注 OopLab 公众号后，回复 nuaa 获取访问密码！</div>
+      <div className='font-bold'>关注 OopLab 公众号，回复校名全称（8个汉字），以获取访问密码。</div>
       <div className='flex mx-4'>
         <input id="password" type='password'
             onKeyDown={(e) => {
